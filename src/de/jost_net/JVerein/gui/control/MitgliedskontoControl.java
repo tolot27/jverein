0e5eee39d1c06b1ff4c5e2bf600f69ea1c8dd1fe
Johann Maierhofer <126261667+JohannMaierhofer@users.noreply.github.com>
2025-03-11 21:36:25 +0100

    Fix Zahlungswege für Sollbuchungen (#762)
    Co-authored-by: Johann Maierhofer <johann.maierhofer@freenet.de>

--- src/de/jost_net/JVerein/gui/control/MitgliedskontoControl.java
+++ src/de/jost_net/JVerein/gui/control/MitgliedskontoControl.java
@@ -230,11 +230,7 @@ public class MitgliedskontoControl extends DruckMailControl
     {
       z = getMitgliedskonto().getZahlungsweg();
     }
-    boolean mitVollzahler = false;
-    if (getMitglied().getValue() != null
-        && ((Mitglied) getMitglied().getValue()).getVollZahlerID() != null)
-      mitVollzahler = true;
-    ArrayList<Zahlungsweg> weg = Zahlungsweg.getArray(mitVollzahler);
+    ArrayList<Zahlungsweg> weg = Zahlungsweg.getArray(false);
 
     zahlungsweg = new SelectInput(weg,
         z == null

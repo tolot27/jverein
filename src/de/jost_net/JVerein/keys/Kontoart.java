49fc6e7394c56d2e0494145d8d65ff6c43403e35
Johann Maierhofer <126261667+JohannMaierhofer@users.noreply.github.com>
2025-07-05 19:13:20 +0200

    Kontoarten fuer Verbindlichkeiten und Forderungen (#913)
    Co-authored-by: Johann Maierhofer <johann.maierhofer@freenet.de>

--- src/de/jost_net/JVerein/keys/Kontoart.java
+++ src/de/jost_net/JVerein/keys/Kontoart.java
@@ -37,7 +37,9 @@ public enum Kontoart
   RUECKLAGE_ERWERB(107, "Rücklage für Gesellschaftsrechte nach § 62 Abs. 1 Nr. 4 AO"),
   VERMOEGEN(108, "Vermögen nach § 62 Abs. 3 und 4 AO"),
   RUECKLAGE_SONSTIG(109, "Sonstige Rücklagen und Vermögen"),
-  LIMIT_RUECKLAGE(200, "-- Limit Rücklage --");
+  LIMIT_RUECKLAGE(200, "-- Limit Rücklage --"),
+  VERBINDLICHKEITEN(201, "Verbindlichkeiten"),
+  FORDERUNGEN(202, "Forderungen");
 
   private final String text;
 

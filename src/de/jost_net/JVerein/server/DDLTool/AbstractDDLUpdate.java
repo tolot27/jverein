b9d2f373279950b06f34c6804e58f0ba607574b2
Marlon Müller <marlon130403@gmail.com>
2025-08-08 08:57:02 +0200

    Revert "DB Update Transaction (#992)" (#1003)
    This reverts commit 5ca798e0b69816fac7aff55b09b25b2cdfb6a764.

--- src/de/jost_net/JVerein/server/DDLTool/AbstractDDLUpdate.java
+++ src/de/jost_net/JVerein/server/DDLTool/AbstractDDLUpdate.java
@@ -85,7 +85,7 @@ public abstract class AbstractDDLUpdate implements IDDLUpdate
     }
     try
     {
-      Logger.info(statement);
+      Logger.debug(statement);
       ScriptExecutor.execute(new StringReader(statement), conn, null);
       if (setVersion)
       {
